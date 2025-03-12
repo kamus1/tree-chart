027f6f2ccf36dac239c5a3605ca425f8a4c043db
kamus1 <benjamincamus9@gmail.com>
2025-03-12 17:23:51 -0300

    fix(tree-chart-core): test fix v4.2

---
+++ .releaserc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    plugins: [
+      [
+        "semantic-release-monorepo",
+        {
+          packages: [
+            "packages/tree-chart-core",
+            "packages/vue3-tree-chart"
+          ]
+        }
+      ]
+    ]
+  };
